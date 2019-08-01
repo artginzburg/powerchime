e58b10a90354183b28f10b0bbd13089bd147a409
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-02 01:32:37 +0300

    again shortened script

--- powerchime.sh
+++ powerchime.sh
@@ -10,17 +10,17 @@ powerchime() {
     echo 'turned off'
     killall $pc
     afplay /System/Library/Sounds/Submarine.aiff
-    local cOnNo=true
-    local cOnAll=false
+    local cOnNo=1
+    local cOnAll=0
   else
     echo 'is running'
     afplay /System/Library/CoreServices/$pc.app/Contents/Resources/connect_power.aif
-    local cOnNo=false
-    local cOnAll=true
+    local cOnNo=0
+    local cOnAll=1
   fi
 
-  defaults write com.apple.$pc ChimeOnNoHardware -bool $cOnNo && \
-  defaults write com.apple.$pc ChimeOnAllHardware -bool $cOnAll
+  defaults write com.apple.$pc ChimeOnNoHardware $cOnNo && \
+  defaults write com.apple.$pc ChimeOnAllHardware $cOnAll
 
   if ! [ $running ]; then
     open -g -a $pc
